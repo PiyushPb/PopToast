8743d2273da63862cc847c3103bdbae98a8e94a8
Piyush Pardeshi <piyushp0512@gmail.com>
2023-11-15 19:46:26 +0530

    License and readme filed added

--- src/index.js
+++ src/index.js
@@ -1,69 +1,65 @@
 const ToastLibrary = (function () {
-    const notifications = document.querySelector(".notifications");
+  const notifications = document.querySelector(".notification-container");
 
-    const addStylesheet = (href) => {
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = href;
-        document.head.appendChild(link);
-    };
-    
-    addStylesheet("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css");
-    
+  const addStylesheet = (href) => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+  };
 
-    const toastDetails = {
-        timer: 5000,
-        success: {
-            icon: 'fa-circle-check',
-            text: 'Success: This is a success toast.',
-        },
-        error: {
-            icon: 'fa-circle-xmark',
-            text: 'Error: This is an error toast.',
-        },
-        warning: {
-            icon: 'fa-triangle-exclamation',
-            text: 'Warning: This is a warning toast.',
-        },
-        info: {
-            icon: 'fa-circle-info',
-            text: 'Info: This is an information toast.',
-        }
-    };
+  addStylesheet(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+  );
 
-    const removeToast = (toast) => {
-        toast.classList.add("hide");
-        if (toast.timeoutId) clearTimeout(toast.timeoutId);
-        setTimeout(() => toast.remove(), 500);
-    };
+  const toastDetails = {
+    timer: 5000,
+    success: {
+      icon: "fa-circle-check",
+    },
+    error: {
+      icon: "fa-circle-xmark",
+    },
+    warning: {
+      icon: "fa-triangle-exclamation",
+    },
+    info: {
+      icon: "fa-circle-info",
+    },
+  };
 
-    const createToast = (message, type) => {
-        const { icon, text } = toastDetails[type];
-        const toast = document.createElement("li");
-        toast.className = `toast ${type}`;
-        toast.innerHTML = `<div class="column">
-                              <i class="fa-solid ${icon}"></i>
-                              <span>${message}</span>
-                           </div>
-                           <i class="fa-solid fa-xmark" onclick="ToastLibrary.removeToast(this.parentElement)"></i>`;
-        notifications.appendChild(toast);
-        toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
-    };
+  const removeToast = (toast) => {
+    toast.classList.add("hide");
+    if (toast.timeoutId) clearTimeout(toast.timeoutId);
+    setTimeout(() => toast.remove(), 500);
+  };
 
-    const popToast = (message, type) => {
-        if (toastDetails[type]) {
-            createToast(message, type);
-        } else {
-            console.error("Invalid toast type");
-        }
-    };
+  const createToast = (message, type) => {
+    const { icon, text } = toastDetails[type];
+    const toast = document.createElement("li");
+    toast.className = `toast-item ${type}`;
+    toast.innerHTML = `<div class="column">
+                                <i class="fa-solid ${icon}"></i>
+                                <span>${message}</span>
+                             </div>
+                             <i class="fa-solid fa-xmark" onclick="ToastLibrary.removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast);
+    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+  };
 
-    return {
-        popToast,
-        removeToast,
-    };
+  const popToast = (message, type) => {
+    if (toastDetails[type]) {
+      createToast(message, type);
+    } else {
+      console.error("Invalid toast type");
+    }
+  };
+
+  return {
+    popToast,
+    removeToast,
+  };
 })();
-
 
 window.popToast = ToastLibrary.popToast;
 window.removeToast = ToastLibrary.removeToast;
